b2f7b4a5ce78c4b91608cd24e98b34c5de53f955
Santiago Suaza Builes <santiagosuazabuiles01@gmail.com>
2021-09-23 22:02:40 -0500

    new files

---
+++ bootstrap_project/functions.js
@@ -0,0 +1,53 @@
+//FUNCION PARA MANIPULAR EL DOM
+function cambiarImagen(params) {
+    document.getElementById("img1").src = ("https://image.goat.com/750/attachments/product_template_pictures/images/060/556/759/original/815560_00.png.png")
+}
+
+
+
+
+/*********VARIABLES*********/
+
+const numero_5 = 5;
+var cadena = "hola mundo";
+let numero_decimal = 10.2;
+
+/*********FUNCIONES********/
+function saludar() {
+  alert("Hola mundo");
+}
+
+function operacion() {
+  sumar(5, 2);
+}
+
+function sumar(num1, num2) {
+  return num1 + num2;
+}
+
+let funcionFlecha =()=>{
+    console.log("esto es una funcion flecha")
+}
+
+/*************CICLOS*********/
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let personas = ["Juan", "Ana", "Andrés", "rubén"];
+for (let i = 0; i < personas.length; i++) {
+  console.log(personas[i]);
+}
+console.log("------FOREACH-------");
+personas.forEach((element) => {
+  console.log(element);
+});
+console.log("------While-------");
+let contador = 0;
+while (contador < personas.length) {
+    console.log(personas[contador]);
+    ++contador;
+    if (contador==2){
+        console.log("terminando recorrido")
+    }
+}
